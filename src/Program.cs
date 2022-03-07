644b333d0e6a5f3f89bb29143ac36b6e01b42efb
karlkoorna <karlkoorna@gmail.com>
2022-03-07 14:37:10 +0200

    Hide console window on startup

--- src/Program.cs
+++ src/Program.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+ConsoleWindow.Hide();
+
 var config = await Utils.LoadConfig<Config>("Glucy.json");
 var builder = WebApplication.CreateBuilder();
 
